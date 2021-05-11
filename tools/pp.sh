2e535b5f4a3d8d0d60ad870d76339570c0d472bc
vortex314 <lieven.merckx@gmail.com>
2021-05-12 01:08:10 +0200

    updating tools

--- tools/pp.sh
+++ tools/pp.sh
@@ -1,1 +1,1 @@
-sudo pppd /dev/ttyUSB0 1000000 192.168.1.1:192.168.1.2 netmask 255.255.255.0 noauth local  dump  nocrtscts persist maxfail 0 holdoff 1 
+sudo pppd /dev/ttyUSB0 1000000 192.168.1.1:192.168.1.2 noauth local debug dump  nocrtscts persist maxfail 0 holdoff 1 
